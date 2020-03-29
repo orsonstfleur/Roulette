47b1e3270aeef347466dc1e277100b0ab72ff735
Orson Saint Fleur <orsonsaintfleurgmail.com>
2020-03-28 22:48:11 -0400

    ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Roulette
+
+![Roulette](thumbnail.png)
+
+## Languages Used:
+### HTML, CSS, JS, Node, EJS
+## CSS credits to Divya Sharma
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. run `node server.js`
+2. Navigate to `localhost:8080`
---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+
+module.exports = {
+
+    'url' : 'mongodb+srv://roulette:roulette@roulette-qznx0.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'roulette'
+};
